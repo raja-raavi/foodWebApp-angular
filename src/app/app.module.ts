bcc9491d47bb0696613ffcde52a4b8c8fd22b73d
raja-raavi <rajaraavi999@gmail.com>
2024-08-01 08:15:57 +0530

    developed navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './site frame work/home/home.component';
+import { NavBarComponent } from './site frame work/nav-bar/nav-bar.component';
+import { FooterComponent } from './site frame work/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
