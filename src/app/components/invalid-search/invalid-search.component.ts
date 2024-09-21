19837ad1a9f7e4283305c4dd78252d0d4cc00b7c
raja-raavi <rajaraavi999@gmail.com>
2024-09-21 21:06:01 +0530

    Implemented page not found

--- src/app/components/invalid-search/invalid-search.component.ts
+++ src/app/components/invalid-search/invalid-search.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-invalid-search',
   templateUrl: './invalid-search.component.html',
-  styleUrls: ['./invalid-search.component.css']
+  styleUrls: ['./invalid-search.component.css'],
 })
 export class InvalidSearchComponent {
+  constructor(private router: Router) {}
 
+  buttonClicked() {
+    this.router.navigate(['']);
+  }
 }
--- src/app/site frame work/error/error.component.ts
+++ src/app/site frame work/error/error.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  styleUrls: ['./error.component.css']
+  styleUrls: ['./error.component.css'],
 })
 export class ErrorComponent {
+  constructor(private router: Router) {}
 
+  buttonClicked() {
+    this.router.navigate(['']);
+  }
 }
